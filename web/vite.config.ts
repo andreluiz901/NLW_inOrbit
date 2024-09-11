4e5a388dbb1e490eeb55fbc1c17222fafad3c4cb
André Luiz <silva.ala82@gmail.com>
2024-09-11 20:36:47 -0300

    fix: fix locale pt-br dayjs

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['dayjs/locale/pt-BR'],
+      external: ['dayjs/locale/pt-br'],
     },
   },
 })
