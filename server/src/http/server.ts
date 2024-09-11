07501c28b04d22f4b8b7f83e9a61ebb51ab35364
André Luiz <silva.ala82@gmail.com>
2024-09-11 17:16:25 -0300

    feat: implements port to env, to be secured and to better deploy

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -1,33 +1,34 @@
-import fastfy from "fastify";
+import fastfy from 'fastify'
 import {
-	serializerCompiler,
-	validatorCompiler,
-	type ZodTypeProvider,
-} from "fastify-type-provider-zod";
-import { createGoalRoute } from "./routes/create-goal";
-import { createCompletionRoute } from "./routes/create-completion";
-import { getPendingRoute } from "./routes/get-pending-goals";
-import { getWeekSummaryRoute } from "./routes/get-week-summary";
-import fastifyCors from "@fastify/cors";
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { createGoalRoute } from './routes/create-goal'
+import { createCompletionRoute } from './routes/create-completion'
+import { getPendingRoute } from './routes/get-pending-goals'
+import { getWeekSummaryRoute } from './routes/get-week-summary'
+import fastifyCors from '@fastify/cors'
+import { env } from '../env'
 
-const app = fastfy().withTypeProvider<ZodTypeProvider>();
+const app = fastfy().withTypeProvider<ZodTypeProvider>()
 
 app.register(fastifyCors, {
-	origin: "*",
-});
+  origin: '*',
+})
 
-app.setValidatorCompiler(validatorCompiler);
-app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
 
-app.register(createGoalRoute);
-app.register(createCompletionRoute);
-app.register(getPendingRoute);
-app.register(getWeekSummaryRoute);
+app.register(createGoalRoute)
+app.register(createCompletionRoute)
+app.register(getPendingRoute)
+app.register(getWeekSummaryRoute)
 
 app
-	.listen({
-		port: 3333,
-	})
-	.then(() => {
-		console.log("Server running!!!");
-	});
+  .listen({
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('Server running!!!')
+  })
